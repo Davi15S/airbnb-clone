54e3113d203341fcd03885f09e22d58736add904
Davi15S <nguyenxd072@gmail.com>
2022-04-21 18:56:55 +0200

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,11 +3,11 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div className='bg-[#f7f7f7] px-5 pt-3'>
-            <div className='py-3'>
+        <div className='bg-[#f7f7f7] px-6 pt-3 md:px-10 xl:px-20 2xl:px-40 2xl:mt-10'>
+            <div className='py-3 gird grid grid-cols-1 xl:grid-cols-4 xl:border-b border-gray-200 xl:pb-10 xl:pt-5 xl:mb-4'>
                 <div>
                     <FooterTitle txt={"Podpora"} />
-                    <div className='grid grid-cols-1'>
+                    <div className='grid grid-cols-1 xl:grid-cols-1 md:grid-cols-3'>
                         <FooterItem txt={"Centrum nápovědy"} />
                         <FooterItem txt={"Bezpečnostní pokyny"} />
                         <FooterItem txt={"Možnosti zrušení rezervace"} />
@@ -15,31 +15,31 @@ function Footer() {
                         <FooterItem txt={"Podpora osob s handicapem"} />
                         <FooterItem txt={"Nahlásit problém v okolí"} />
                     </div>
-                    <div className="w-full h-[1px] bg-gray-200 my-4" />
+                    <div className="w-full h-[1px] bg-gray-200 my-4 xl:hidden" />
                 </div>
                 <div>
-                    <FooterTitle txt={"komunita"} />
-                    <div className='grid grid-cols-1'>
+                    <FooterTitle txt={"Komunita"} />
+                    <div className='grid grid-cols-1 xl:grid-cols-1 md:grid-cols-3'>
                         <FooterItem txt={"Airbnb.org: pomoc v nouzi"} />
                         <FooterItem txt={"Podpořit afghánské uprchlíky"} />
                         <FooterItem txt={"Boj proti diskriminaci"} />
                     </div>
-                    <div className="w-full h-[1px] bg-gray-200 my-4" />
+                    <div className="w-full h-[1px] bg-gray-200 my-4 xl:hidden" />
                 </div>
                 <div>
                     <FooterTitle txt={"Hostitelství"} />
-                    <div className='grid grid-cols-1'>
+                    <div className='grid grid-cols-1 xl:grid-cols-1 md:grid-cols-3'>
                         <FooterItem txt={"Vyzkoušet hostitelství"} />
-                        <FooterItem txt={"BezpAirCover: krytí pro hostiteleečnostní"} />
+                        <FooterItem txt={"BezpAirCover: krytí pro hostitele"} />
                         <FooterItem txt={"Projít si hostitelské zdroje"} />
                         <FooterItem txt={"Navštívit komunitní fórum"} />
                         <FooterItem txt={"Jak hostit zodpovědně"} />
                     </div>
-                    <div className="w-full h-[1px] bg-gray-200 my-4" />
+                    <div className="w-full h-[1px] bg-gray-200 my-4 xl:hidden" />
                 </div>
                 <div>
                     <FooterTitle txt={"Informace"} />
-                    <div className='grid grid-cols-1'>
+                    <div className='grid grid-cols-1 xl:grid-cols-1 md:grid-cols-3'>
                         <FooterItem txt={"Redakce"} />
                         <FooterItem txt={"Víc o nových funkcích"} />
                         <FooterItem txt={"Dopis od našich zakladatelů"} />
@@ -47,19 +47,20 @@ function Footer() {
                         <FooterItem txt={"Investoři"} />
                         <FooterItem txt={"Airbnb Luxe"} />
                     </div>
-                    <div className="w-full h-[1px] bg-gray-200 my-4" />
-                </div>
-                <div className='flex my-1 space-x-7'>
-                    <div className='flex items-center group cursor-pointer space-x-2'>
-                        <GlobeAltIcon className='h-5 group-hover:opacity-90 text-[#222222]'/>
-                        <p className='h-[21px] text-[#222222] border-[#222222] group-hover:opacity-90 border-b font-semibold'>Čeština (CZ)</p>
-                    </div>
-                    <div className='flex space-x-2 group cursor-pointer'>
-                        <div>Kč</div>
-                        <div className='h-[21px] text-[#222222] border-[#222222] group-hover:opacity-90 border-b font-semibold'>CZK</div>
-                    </div>
+                    <div className="w-full h-[1px] bg-gray-200 my-4 xl:hidden" />
                 </div>
             </div>
+            <div className='flex my-1 space-x-7'>
+                <div className='flex items-center group cursor-pointer space-x-2'>
+                    <GlobeAltIcon className='h-5 group-hover:opacity-90 text-[#222222]' />
+                    <p className='h-[21px] text-[#222222] border-[#222222] group-hover:opacity-90 border-b font-semibold'>Čeština (CZ)</p>
+                </div>
+                <div className='flex space-x-2 group cursor-pointer'>
+                    <div>Kč</div>
+                    <div className='h-[21px] text-[#222222] border-[#222222] group-hover:opacity-90 border-b font-semibold'>CZK</div>
+                </div>
+            </div>
+            <p className='text-center py-4 font-semibold text-[#222222]'>This site was created just for educational porpuses.</p>
         </div>
     )
 }
@@ -72,7 +73,7 @@ function FooterTitle({ txt }) {
 
 function FooterItem({ txt }) {
     return (
-        <span className='hover:border-b h-[21px] my-1 cursor-pointer text-[#222222] border-[#222222] max-w-fit'>{txt}</span>
+        <span className='hover:border-b h-[21px] my-2 cursor-pointer text-[#222222] border-[#222222] max-w-fit'>{txt}</span>
     )
 }
 
--- components/Hostitelstvi.js
+++ components/Hostitelstvi.js
@@ -8,10 +8,10 @@ function Hostitelstvi() {
                 <div className='md:hidden'>
                     <Image src={"https://a0.muscache.com/im/pictures/8262da55-f57f-460a-88cd-67a1d07b6f80.jpg?im_w=320"} layout="fill" />
                 </div>
-                <div className='md:flex hidden xl:hidden'>
+                <div className='md:flex hidden 2xl:hidden'>
                     <Image src={"https://a0.muscache.com/im/pictures/0903a029-e330-41f6-add2-652954f62185.jpg?im_w=720"} layout="fill" />
                 </div>
-                <div className='xl:flex hidden'>
+                <div className='2xl:flex hidden'>
                     <Image src={"https://a0.muscache.com/im/pictures/0528b0f7-4c0c-47bc-9786-d91454f531ba.jpg?im_w=1200"} layout="fill" className='rounded-3xl'/>
                 </div>
 
